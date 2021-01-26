230ffa361d8a21a03e30e1101db37c44aee48431
benanil <48527900+benanil@users.noreply.github.com>
2021-01-26 23:58:26 +0300

    Add files via upload
    just paint bruh

---
+++ RaycastPainter.cs
@@ -0,0 +1,28 @@
+﻿
+using UnityEngine;
+
+namespace AnilTools.Paint
+{
+    public class RaycastPainter : MonoBehaviour
+    {
+        public Color color;
+        public LayerMask layerMask;
+
+        public int radius;
+        public bool erase;
+
+        private void Update()
+        {
+            if (InputPogo.MouseHold)
+            {
+                if (Mathmatic.RaycastFromCamera(out RaycastHit hit, Mathmatic.FloatMaxValue, layerMask))
+                {
+                    if (erase)
+                        Painter.PaitCircale(hit, radius, color);
+                    else
+                        Painter.EraseCircale(hit, radius);
+                }
+            }
+        }
+    }
+}
