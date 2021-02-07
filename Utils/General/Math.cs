7a463352bf6f5ef69a5d76eba481bc3f605a0873
benanil <48527900+benanil@users.noreply.github.com>
2021-02-07 23:03:05 +0300

    Update Math.cs

--- Utils/General/Math.cs
+++ Utils/General/Math.cs
@@ -1,4 +1,4 @@
-﻿
+
 using UnityEngine;
 using UnityEngine.AI;
 using AnilTools.AnilEditor;
@@ -12,7 +12,7 @@ namespace AnilTools
         internal const byte Hundered = 100;
         private const float Half = .5f;
         public static Ray Ray => camera.ScreenPointToRay(Input.mousePosition);
-        public static Ray RayMidleScreen => new Ray(camera.transform.position,camera.transform.forward * Hundered);
+        public static Ray RayMidleScreen => new Ray(camera.transform.position, camera.transform.forward * Hundered);
         public static Camera camera
         {
             get
@@ -40,7 +40,7 @@ namespace AnilTools
         /// <summary> if value is plus value returns minus othervise return plus </summary>
         public static float Inverse(this float value)
         {
-            return value < 0 ? Mathf.Abs(value) : -value;    
+            return value < 0 ? Mathf.Abs(value) : -value;
         }
 
         /// <summary> if value is plus value returns minus othervise return plus </summary>
@@ -49,7 +49,12 @@ namespace AnilTools
             return value < 0 ? Mathf.Abs(value) : -value;
         }
 
-        public static float Clamp( float value, float min, float max)
+        public static float Remap(this float value, float FirstMin, float FirstMax, float SecondMin, float SecondMax)
+        {
+            return (value - FirstMin) / (FirstMax - FirstMin) * (SecondMax - SecondMin) + SecondMin;
+        }
+
+        public static float Clamp(float value, float min, float max)
         {
             if (value < min)
                 return min;
@@ -80,7 +85,22 @@ namespace AnilTools
         }
 
         #region Min Max
-        
+
+        // instagramda 2 tarafa girilen oyun yüzdesini hesaplanak için kullanılan sistem
+        public static Tuple2<float, float>Calculate2Percent(float firstCount, float secondCount)
+        {
+            var OnePice = (firstCount + secondCount) / 100;
+            return new Tuple2<float, float>(firstCount / OnePice, secondCount / OnePice);
+        }
+
+        // 3 partiye velilen oy listesi
+        public static Tuple3<float, float , float> Calculate3Percent(float firstCount, float secondCount,float thirdCount)
+        {
+            var OnePice = (firstCount + secondCount + thirdCount) / 100;
+            
+            return new Tuple3<float, float , float>(firstCount/OnePice, secondCount/OnePice, thirdCount/OnePice);
+        }
+
         /// <summary>
         /// bu değerden yukarı şıkmasını engeller </summary>
         public static int Max(this int value,int max = 1)
@@ -119,7 +139,7 @@ namespace AnilTools
 
         public static float Min(this float value, float min)
         {
-            if (value > min) return min;
+            if (value < min) return min;
             else return value;
         }
 
@@ -170,6 +190,13 @@ namespace AnilTools
             return pos;
         }
 
+
+        public static Vector3 ClosestPointToCircale(Vector3 origin, Vector3 target, float Radius)
+        {
+            var ray = new Ray(origin, (origin - target).normalized);
+            return ray.GetPoint(-Radius);
+        }
+
         public static Vector3 FindSamplePos(Vector3 pos, Vector3 randomPos, float maxy)
         {
             var SamplePos = Vector3.zero;
@@ -192,28 +219,36 @@ namespace AnilTools
             return pos;
         }
 
-        static Texture2D texture2D;
-        static Renderer renderer;
-        static Vector2 pCoord;
+        public static Vector3 Direction(Vector3 origin, Vector3 Point)
+        {
+            return (origin - Point).normalized;
+        }
+
+        private static class RointInfo
+        { 
+            public static Texture2D texture2D;
+            public static Renderer renderer;
+            public static Vector2 pCoord;
+        }
 
         public static Color RaycastPointColor(RaycastHit hit)
         {
-            renderer = hit.collider.GetComponent<MeshRenderer>();
-            if (!renderer)
+            RointInfo.renderer = hit.collider.GetComponent<MeshRenderer>();
+            if (!RointInfo.renderer)
                 return Color.white;
 
-            texture2D = renderer.material.mainTexture as Texture2D;
+            RointInfo.texture2D = RointInfo.renderer.material.mainTexture as Texture2D;
             
-            if (!texture2D.isReadable)
+            if (!RointInfo.texture2D.isReadable)
             {
                 Debug2.Log("texture is not readable");
                 return Color.white;
             }
-            
-            pCoord = hit.textureCoord;
-            pCoord.x *= texture2D.width;
-            pCoord.y *= texture2D.height;
-            return texture2D.GetPixel(Mathf.FloorToInt(pCoord.x * renderer.material.mainTextureScale.x), Mathf.FloorToInt(pCoord.y * renderer.material.mainTextureScale.y));
+
+            RointInfo.pCoord = hit.textureCoord;
+            RointInfo.pCoord.x *= RointInfo.texture2D.width;
+            RointInfo.pCoord.y *= RointInfo.texture2D.height;
+            return RointInfo.texture2D.GetPixel(Mathf.FloorToInt(RointInfo.pCoord.x * RointInfo.renderer.material.mainTextureScale.x), Mathf.FloorToInt(RointInfo.pCoord.y * RointInfo.renderer.material.mainTextureScale.y));
         }
 
         /// <summary>
@@ -236,26 +271,16 @@ namespace AnilTools
             return dist < range;
         }
 
-        public static bool RaycastFromCamera(out RaycastHit hit)
+        public static bool RaycastFromCamera(out RaycastHit hit, float MaxDistance = 2048, int layerMask = -1, QueryTriggerInteraction queryTriggerInteraction = QueryTriggerInteraction.Ignore)
         {
-            return Physics.Raycast(Ray, out hit);
-        }
-
-        public static bool RaycastFromCamera(out RaycastHit hit, float MaxDistance, LayerMask layerMask)
-        {
-            return Physics.Raycast(Ray, out hit, MaxDistance, layerMask);
-        }
-
-        public static bool RaycastFromCameraMidle(out RaycastHit hit)
-        {
-            return Physics.Raycast(RayMidleScreen, out hit);
+            return Physics.Raycast(Ray, out hit, MaxDistance, layerMask, queryTriggerInteraction);
         }
 
         public static bool RaycastFromCameraMidle(out RaycastHit hit, float maxDistance, LayerMask layerMask)
         {
             return Physics.Raycast(RayMidleScreen, out hit, maxDistance , layerMask);
         }
-
     }
 
 }
+
